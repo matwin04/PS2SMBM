197df54f0ccf2e426c249bbb161289d82741ea2c
Matt Winer <mattwiner04@gmail.com>
2024-12-17 12:17:35 -0800

    2. Starting Over

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 // Created by mwiner on 12/13/24.
 //
 #include <QApplication>
+
 int main(int argc,char *argv[]){
     QApplication app(argc,argv);
-    return app.exec()
+    return app.exec();
 }
